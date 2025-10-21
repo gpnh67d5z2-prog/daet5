21992a3da1cde437b13a771a78aff58ab2667581
gpnh67d5z2-prog <gpnh67d5z2@privaterelay.appleid.com>
2025-10-21 08:45:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+    const user = 'SIXSEVEN';
+    const points = 67;
+    const msg = `Привет, ${user}! У тебя ${points} очков.`;
+    console.log(msg);
+
+    // Арифметические операции
+    let a = 10;
+    let b = 3;
+    console.log(a + b); // 13
+    console.log(a - b); // 7
+    console.log(a * b); // 30
+    console.log(a / b); // 3.333...
+    console.log(a % b); // остаток от деления
+
